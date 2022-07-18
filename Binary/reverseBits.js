39acb52c3826f115bebce09c3ea8e757513b0f74
DBueno1464 <DerrickBueno00@gmail.com>
2022-07-18 07:19:37 -0400

    added an overview to hash tables

---
+++ Binary/reverseBits.js
@@ -0,0 +1,17 @@
+/*
+
+Reverse bits of a given 32 bits unsigned integer.
+
+*/
+
+
+var reverseBits = function(n) {
+    console.log(n)
+};
+
+let n1 = 0b00000010100101000001111010011100; // answer is 964176192 (00111001011110000010100101000000)
+
+let n2 = 0b11111111111111111111111111111101; // answer 3221225471 (10111111111111111111111111111111)
+
+// reverseBits(n1);
+// reverseBits(n2);
