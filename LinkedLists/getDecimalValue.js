989fd60e242ed1152dde5d552fd9b872b2c4c372
DBueno1464 <DerrickBueno00@gmail.com>
2022-09-02 12:21:49 -0400

    fixed getDecimalValue.js to work on vscode, not only leetcode code space

--- LinkedLists/getDecimalValue.js
+++ LinkedLists/getDecimalValue.js
@@ -8,17 +8,13 @@ The most significant bit is at the head of the linked list.
 
 https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
 
-
-NOTE: COPY AND PASTE UNCOMMENTED CODE INTO LEETCODE CODE SPACE.
 */
 
 //  Definition for singly-linked list.
-// function ListNode(val, next) {
-//     this.val = (val === undefined ? 0 : val)
-//     this.next = (next === undefined ? null : next)
-
-//     return this, console.log(this);
-// }
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
 
 var getDecimalValue = function (head) {
     let currentNode = head;
@@ -28,11 +24,22 @@ var getDecimalValue = function (head) {
         currentNode = currentNode.next;
     }
     
-    return parseInt(binaryString, 2);
+    return parseInt(binaryString, 2), console.log(parseInt(binaryString, 2));
 };
 
-// let head1 = [1, 0, 1];  // answer is 5; (101) in base 2 = (5) in base 10
-// let head2 = [0];        // answer is 0;
+let head1 = [1, 0, 1];  // answer is 5; (101) in base 2 = (5) in base 10
 
-// getDecimalValue(head1);
-// getDecimalValue(head2);
+let head1Node1 = new ListNode(1);
+let head1Node2 = new ListNode(0);
+let head1Node3 = new ListNode(1);
+
+head1Node1.next = head1Node2;
+head1Node2.next = head1Node3;
+
+// getDecimalValue(head1Node1);
+
+let head2 = [0];        // answer is 0;
+
+let head2Node1 = new ListNode(0);
+
+getDecimalValue(head2Node1);
